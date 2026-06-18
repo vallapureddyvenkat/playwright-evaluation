86aa93a8457f5553a6b64cdb43b32ba839c38a8e
Venkat Reddy Vallapureddy <vallapureddy.venkat@gmail.com>
2026-06-19 00:26:13 +0530

    Assignment 2 CI/CD Integration

--- Assignment2/playwright.config.ts
+++ Assignment2/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure'
   },
